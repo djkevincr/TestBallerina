87bc866f49233504f33c35f16fc49f56ebc7eba7
Kevin Ratnasekera <djkevincr@yahoo.com>
2018-01-21 22:00:43 +0530

    Improve resource clearance

--- src/test/java/org/ballerinalang/sample/BallerinaServiceTest.java
+++ src/test/java/org/ballerinalang/sample/BallerinaServiceTest.java
@@ -21,6 +21,7 @@ package org.ballerinalang.sample;
 import org.ballerinalang.containers.docker.BallerinaDockerClient;
 import org.ballerinalang.containers.docker.exception.BallerinaDockerClientException;
 import org.ballerinalang.containers.docker.impl.DefaultBallerinaDockerClient;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
@@ -30,7 +31,6 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.time.Duration;
-
 
 /**
  * Sample case to test Ballerina Service inside Dockerized environment.
@@ -45,30 +45,42 @@ public class BallerinaServiceTest {
     private static final int SERVICE_STARTUP_TIMEOUT = 60;
     private static final String BALLERINA_VERSION = "0.95.6";
     private String url = "http://localhost:{port}/hello";
-    public GenericContainer ballerinaService;
+    private GenericContainer ballerinaService;
 
     @Before
     public void setup() throws BallerinaDockerClientException, IOException, InterruptedException {
+        // Set Base Ballerina image version - public docker hub released version.
         System.setProperty("ballerina.version", BALLERINA_VERSION);
         this.dockerClient = new DefaultBallerinaDockerClient();
         Path servicePath = Paths.get(BallerinaServiceTest.class.getClassLoader()
                 .getResource("helloWorldService.bal").getPath());
+        // Build child docker image using Service bal file and Base image.
         String createdImageName = dockerClient.createMainImage("TestPackage", null, servicePath,
                 IMAGE_NAME, IMAGE_VERSION);
         assert createdImageName != null;
+        // Spin up container from created docker image in previous step.
         this.ballerinaService = new GenericContainer(DOCKER_CONTAINER_NAME)
                 .withExposedPorts(SERVICE_PORT)
                 .waitingFor(new ServiceStartupLogWaitStrategy())
                 .withStartupTimeout(Duration.ofSeconds(SERVICE_STARTUP_TIMEOUT));
+        // Start the container.
         this.ballerinaService.start();
     }
 
 
     @Test
     public void testService() {
+        // Write Custom test cases accordingly.
         String invocationURL = this.url.replace("{port}",
                 ballerinaService.getMappedPort(SERVICE_PORT).toString());
         Assert.assertEquals(HTTPUtil.sendGet(invocationURL), "Hello, World!");
     }
+
+    @After
+    public void tearDown(){
+        // Stop container.
+        this.ballerinaService.stop();
+    }
+
 
 }
